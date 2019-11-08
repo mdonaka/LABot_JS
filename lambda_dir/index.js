ee1ab368cd2157a6e0b518556148f9982f58bc32
mdonaka <4ryo2-meron-math@ezweb.ne.jp>
2019-11-08 23:21:16 +0900

    add post request to slack

--- lambda_dir/index.js
+++ lambda_dir/index.js
@@ -1,8 +1,30 @@
 
-// const decrypter = require("./env")
+//const decrypter = require("./env")
 const decrypter = require("./../env_local")
+
+const request = require("request");
+
+function doRequest(options) {
+	return new Promise(function (resolve, reject) {
+		request.post(options, function (error, res, body) {
+			if (!error && res.statusCode == 200) {
+				resolve(body);
+			} else {
+				reject(error);
+			}
+		});
+	});
+}
 
 exports.handler = async (event, context, callback) => {
 	const URL = await decrypter.get("WebhookURL");
-	return URL;
+
+	const data = {
+		url: URL,
+		headers: {"Content-type": "application/json"},
+		json: {text: "Hello World from nodejs"}
+	};
+	const res = await doRequest(data);
+
+	return {statusCode:200, res:res};
 };
